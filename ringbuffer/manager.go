d5e2f824542a24d36ed42d7d9b0dfcba82ffe3f6
Bryan-Kirk Reinhardt <composer22@hotmail.com>
2015-05-16 21:10:42 -0700

    Rename file

--- ringbuffer/manager.go
+++ ringbuffer/manager.go
@@ -1,16 +1,18 @@
 package ringbuffer
 
+// Manager is a simple wrapper around the various compoments for managing a ringbuffer.
 type Manager struct {
 	Leader   *Sequencer
 	Follower *Sequencer
 }
 
-// ManagerNew instatiates and returns a new Manager
+// ManagerNew instatiates and returns a new Manager.
 func ManagerNew(size int64) *Manager {
 	m := &Manager{
 		Leader:   SequencerNew(size, nil, true),
 		Follower: SequencerNew(size, nil, false),
 	}
+	// Set the dependencies.
 	m.Leader.SetDependency(m.Follower)
 	m.Follower.SetDependency(m.Leader)
 	return m
